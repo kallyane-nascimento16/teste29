70ed0351e863ae66c1b76654776bbf428614164d
kallyane-nascimento16 <110573013+kallyane-nascimento16@users.noreply.github.com>
2022-10-05 20:50:47 -0300

    Add files via upload

---
+++ cadastro_get_set/cadastro_get/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cadastro_get
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            cad p = new cad();
+            p.Nome1= "lucas";
+            Console.WriteLine(p.Nome1);
+
+            cad a = new cad();
+            a.Nome2 = "romulo";
+            Console.WriteLine(a.Nome2);
+
+            cad b = new cad();
+            b.Nome3 = "kallyane";
+            Console.WriteLine(a.Nome3);
+
+            cad c = new cad();
+            c.Nome4 = "vitor";
+            Console.WriteLine(c.Nome4);
+
+            cad d = new cad();
+            d.Nome5 = "jaqueline";
+            Console.WriteLine(d.Nome5);
+
+
+
+
+        }
+    }
+}
---
+++ cadastro_get_set/cadastro_get/cad.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cadastro_get
+{
+    internal class cad
+    {
+        private string nome1,nome2,nome3,nome4,nome5;
+
+        public string Nome1
+        {
+            get { return "lucas"; }
+            set { nome1 = value; }
+
+           
+        }
+        public string Nome2
+        {
+            get { return "romulo"; }
+            set { nome2 = value; }
+
+
+        }
+        public string Nome3
+        {
+            get { return ""; }
+            set { nome3 = value; }
+
+
+        }
+        public string Nome4
+        {
+            get { return ""; }
+            set { nome4 = value; }
+
+
+        }
+        public string Nome5
+        {
+            get { return ""; }
+            set { nome5 = value; }
+
+
+        }
+
+
+
+    }
+}
